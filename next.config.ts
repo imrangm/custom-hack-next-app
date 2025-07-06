41b09c2d4bb6b8a2c787e3431229af239625a5d0
Imran Mustafa <imzs@hotmail.com>
2025-07-06 11:41:30 +0400

    Fix Next.js configuration by removing lingo compiler

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
-import lingoCompiler from "lingo.dev/compiler";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-// for Lingo.dev Compiler configuration see https://lingo.dev/en/compiler/frameworks/nextjs
-export default lingoCompiler.next({
-  sourceLocale: "en",
-  targetLocales: ["es", "fr", "de"],
-  models: "lingo.dev",
-})(nextConfig);
+export default nextConfig;
